e559e947844a7f7acfb67615a757f3e6d4626d12
elizeu-lima <elizeucoca@gmail.com>
2022-08-30 18:51:47 -0300

    inicial commit

---
+++ src/app/mock-api/common/user/data.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export const user = {
+    id    : 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
+    name  : 'Helberty',
+    email : 'marco@marr.com.br',
+    avatar: 'assets/images/avatars/hilberty.jpeg',
+    status: 'online'
+};
